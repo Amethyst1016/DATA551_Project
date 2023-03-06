64c646f3cbb64586424b42f5818a792f5a7098d0
Amethyst1016 <75198440+Amethyst1016@users.noreply.github.com>
2023-03-06 00:05:03 -0800

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -3,20 +3,77 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import altair as alt
+import pandas as pd
+import yfinance as yf
+from functools import reduce
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-from data_processing import *
+from datetime import date
+import datetime
+from dateutil.relativedelta import relativedelta
+
+last_year_today = str(date.today() - relativedelta(years=1))
+index_dict = {
+    # US Market:
+    'S&P_original': '^GSPC', 
+    'S&P_energy': '^GSPE', 
+    'S&P_industry': '^SP500-20', 
+    'S&P_consumer': '^SP500-30',
+    # London Market:
+    'FTSE_100': '^FTSE', 
+    # Europe Market:
+    'Euro_Stoxx_50': '^STOXX50E', 
+    # Hong Kong Market:
+    'HANG_SENG': '^HSI', 
+    # Tokyo Market:
+    'Nikkei_225': '^N225'
+}
+
+def get_daily_data(dicts=index_dict, category='Close', start_date=last_year_today):
+    """
+    Parameters:
+        dicts (dict, optional): The dictionary of global index. Default including 8 indices.
+        category (str, optional): The category of data to include in the data frame. 
+                Options are:
+                'Open', 'High', 'Low', 'Close' (default), 'Adj Close', 'Volume'
+        start_date (str, optional): The start date of the data. Default is last year today.
+    """
+    index_name = list(dicts.keys())
+    index_name.insert(0, 'date')
+    df_total = pd.DataFrame(columns=index_name)
+    dfs = []
+    for index in dicts.keys():
+        symbol = dicts[index]
+        df = yf.download([symbol], group_by='ticker', interval='1d', start=start_date, progress=False)
+        df = df[[category]].reset_index()
+        df = df.rename(columns = {'Close': index})
+        df['Date'] = df['Date'].astype(str).str[:10]
+        df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+        dfs.append(df)
+
+    # Merge all dataframes based on date column
+    df_total = reduce(lambda  left,right: pd.merge(left, right, on=['Date'], how='outer'), dfs)
+    df_total = df_total.sort_values('Date')
+
+    dates = [i.strftime("%Y-%m-%d") for i in pd.date_range(start=last_year_today, end=date.today(), freq='D')]
+    df_date = pd.DataFrame(dates, columns=['Date'])
+    df_date['Date'] = pd.to_datetime(df_date['Date'], format='%Y-%m-%d')
+    df_total = df_date.merge(df_total, on='Date', how='outer')
+    
+    return df_total
+
+
+def select_time_range(df, time_range):
+    # take time_range as the number of rows to select
+    # start from the last row
+    end = df['Date'].max()
+    d = datetime.timedelta(days=time_range)
+    start = end - d
+    select = (df['Date'] >= start) & (df['Date'] <= end)
+    return df.loc[select]
 
 # get data
 df_line_chart = get_daily_data()
-
-# general info
-df_compare = select_time_range(df_line_chart.dropna(), 2)
-cols = df_compare.columns.tolist()[1:]
-growth_rates = {}
-for i in cols:
-    ls = df_compare[i].tolist()
-    growth_rates[i] = round((ls[1]-ls[0])/ls[0]*100, 4)
 
 df_line_chart['Date'] = pd.to_datetime(df_line_chart['Date'])
 df_line_chart.set_index('Date', inplace=True)
@@ -49,6 +106,7 @@ def top_5_company(time_range, df):
 
 # css
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 US_Market = {'font-size': '15px', 'text-align':'left', 'color':'#036d90', 'margin-left':'20px'}
 other_Market = {'font-size': '15px', 'text-align':'left', 'margin-left':'20px'}
 labels = {'display':'inline', 'font-size':'20px', 'margin-left':'5px'}
@@ -59,20 +117,12 @@ app.layout = html.Div([
     html.Div([
         # growth rate info
         html.Div([
-            html.P('Growth Rate Summary\n of '+str(df_compare['Date'].max())[:10], style={'font-size': '25px', 'text-align':'center'}),
-            html.P('US Market', style=US_Market),
-            html.P(f'- SP500: {growth_rates[cols[0]]}%', style=US_Market),
-            html.P(f'- SP500 Energy: {+growth_rates[cols[1]]}%', style=US_Market),
-            html.P(f'- SP500 Industry: {+growth_rates[cols[2]]}%', style=US_Market),
-            html.P(f'- SP500 Consumer: {+growth_rates[cols[3]]}%', style=US_Market),
-            html.P('London Market', style=other_Market),
-            html.P(f'- FTSE_100: {+growth_rates[cols[4]]}%', style=other_Market),
-            html.P('Europe Market', style=other_Market),
-            html.P(f'- Euro_Stoxx_50: {+growth_rates[cols[5]]}%', style=other_Market),
-            html.P('Hong Kong Market', style=other_Market),
-            html.P(f'- HANG_SENG: {+growth_rates[cols[6]]}%', style=other_Market),
-            html.P('Toyko Market', style=other_Market),
-            html.P(f'- Nikkei_225: {+growth_rates[cols[7]]}%', style=other_Market),
+            html.P('Summary of Stock Symbol', style={'font-size': '25px', 'text-align':'center'}),
+            html.P('SP500 - US Market', style=US_Market),
+            html.P('FTSE_100 - London Market', style=other_Market),
+            html.P('Euro_Stoxx_50 - Europe Market', style=other_Market),
+            html.P('HANG_SENG - Hong Kong Market', style=other_Market),
+            html.P('Nikkei_225 - Toyko Market', style=other_Market),
         ], style={'border-style':'solid', 'border-color':'#a1979e', 'margin':'10px'}),
         # radio
         html.Div([
